2a1a6b1218c6e45ac9efb661b582299d29034304
pangbo <dhc-bo.pang@dhc.com.cn>
2020-03-10 12:45:12 +0800

    1.项目准备 (rewire webpack config, npm srript )

---
+++ script/overrides-config.base.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const poststylus = require('poststylus');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+
+module.exports.stylusLoaderOptionsPlugin = new webpack.LoaderOptionsPlugin({
+    options: {
+        stylus: {
+            use: [
+                poststylus([
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                        browsers: [
+                          '>1%',
+                          'last 4 versions',
+                          'Firefox ESR',
+                          'not ie < 9', // React doesn't support IE8 anyway
+                        ],
+                        flexbox: 'no-2009',
+                      })
+                ])
+            ]
+        }
+    }
+});
+
+module.exports.rootPath = resolve('src');
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
