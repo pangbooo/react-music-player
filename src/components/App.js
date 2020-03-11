2499c63da8b283e0028279054b5f1a26f436d9ce
pangbo <dhc-bo.pang@dhc.com.cn>
2020-03-11 10:18:22 +0800

    2.路由&字体图标

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect, NavLink } from 'react-router-dom';
+import logo from '../assets/imgs/logo.png';
+import '../assets/stylus/reset.styl';
+import './App.styl';
+
+import Recommend from "./recommend/Recommend"
+import Ranking from "./ranking/Ranking"
+import Search from "./search/Search"
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <header className='app-header'>
+            <img src={logo} className='app-logo' alt='app-logo'/>
+            <h1 className='app-title'>Mango Music</h1>
+        </header>
+        <div className='music-tab'>
+          <div className='tab-item selected'>
+              <NavLink to='/recommend' className='nav-link'>
+                <span>推荐</span>
+              </NavLink>
+          </div>
+          <div className='tab-item'>
+            <NavLink to='/ranking' className='nav-link'>
+              <span>排行榜</span>
+            </NavLink>
+          </div>
+          <div className='tab-item'>
+              <NavLink to='/search' className='nav-link'>
+                <span>搜索</span>
+              </NavLink>
+          </div>
+        </div>
+
+        <div className='music-view'>
+          {/* 
+            Switch组件用来选择最近的一个路由，否则最后一个没有指定path的路由也会显示
+            Redirect重定向到列表页
+          */}
+          <Switch>
+              <Route path='/recommend' component={Recommend}/>
+              <Route path="/ranking" component={Ranking} />
+              <Route path="/search" component={Search} />
+              <Redirect from='/' to='/recommend'/>
+              <Route component={Recommend} />
+          </Switch>
+        </div>
+      </div>
+      </Router>
+  );
+}
+
+export default App;
