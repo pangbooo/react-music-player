bf0eeef06992f92886f0cb9acb05f46604c764f6
pangbo <dhc-bo.pang@dhc.com.cn>
2020-04-07 16:51:54 +0800

    (七)歌手页-歌手详情

---
+++ src/components/singer/Singer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./singer.styl"
+
+class Singer extends React.Component {
+    render() {
+        return (
+            <div className="music-singer">
+            </div>
+        );
+    }
+}
+
+export default Singer
